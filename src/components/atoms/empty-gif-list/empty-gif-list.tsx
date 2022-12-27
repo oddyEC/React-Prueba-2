b223865e3473ac7ea1d37c33bb9a9e40fbd6770c
Diego Márquez <57378696+oddyEC@users.noreply.github.com>
2022-12-27 10:23:00 -0500

    atomic empty gif lists

---
+++ src/components/atoms/empty-gif-list/empty-gif-list.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./empty-gif-list.scss";
+
+const EmptyGifList = () => {
+  return (
+    <div>
+      <p>No posee gifs</p>
+    </div>
+  );
+};
+
+export default EmptyGifList;
