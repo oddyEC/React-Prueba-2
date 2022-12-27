0d4547bb77e31a267f61f50bcb15bd2e50fa2e66
Diego Márquez <57378696+oddyEC@users.noreply.github.com>
2022-12-27 10:59:52 -0500

    changing app to tsx, first test

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import "./app.scss";
+import GifGaleryPage from "./components/pages/gif-galery-page/gif-galery-page";
+
+const App = () => {
+  return (
+    <div className="app">
+      <GifGaleryPage />
+    </div>
+  );
+};
+
+export default App;
